91577a91e815cd4913279d0489c38698d6ed9591
DeokJong <novelss5573@gmail.com>
2026-05-23 12:15:22 +0900

    feat: init vpc modules

---
+++ foundation/vpc/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "deokjong_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "deokjong"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+  private_subnets = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20"]
+
+  enable_ipv6                  = true
+  public_subnet_ipv6_prefixes  = ["0", "1", "2"]
+  private_subnet_ipv6_prefixes = ["3", "4", "5"]
+
+  public_subnet_enable_dns64                                    = false
+  public_subnet_enable_resource_name_dns_aaaa_record_on_launch  = false
+  private_subnet_enable_dns64                                   = false
+  private_subnet_enable_resource_name_dns_aaaa_record_on_launch = false
+
+  public_subnet_suffix  = "rtb-public"
+  private_subnet_suffix = "rtb-private"
+
+  public_subnet_names = [
+    "deokjong-subnet-public1-ap-northeast-2a",
+    "deokjong-subnet-public2-ap-northeast-2b",
+    "deokjong-subnet-public3-ap-northeast-2c",
+  ]
+
+  private_subnet_names = [
+    "deokjong-subnet-private1-ap-northeast-2a",
+    "deokjong-subnet-private2-ap-northeast-2b",
+    "deokjong-subnet-private3-ap-northeast-2c",
+  ]
+
+  vpc_tags = {
+    Name = "deokjong-vpc"
+  }
+
+  igw_tags = {
+    Name = "deokjong-igw"
+  }
+
+  private_subnet_tags = {
+    "karpenter.sh/discovery/private"  = "-"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  public_subnet_tags = {
+    "karpenter.sh/discovery/public" = "-"
+    "kubernetes.io/role/elb"        = "1"
+  }
+
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+}
