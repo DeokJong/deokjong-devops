087c7a24371cc872e8de3305b6e118f6f2c62aa7
DeokJong <novelss5573@gmail.com>
2026-06-14 04:26:45 +0900

    chore: terraform provider 제거

--- terraform/platform/eks/cluster/terraform.tf
+++ terraform/platform/eks/cluster/terraform.tf
@@ -3,23 +3,9 @@ terraform {
     aws = {
       version = "~> 6.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
   }
 }
 
 provider "aws" {
   region = "ap-northeast-2"
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = ephemeral.aws_eks_cluster_auth.this.token
-}
-
-ephemeral "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
 }
