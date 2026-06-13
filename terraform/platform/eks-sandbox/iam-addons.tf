3c89e73d026ddf1a7f5db3de9632dbd06c8241e4
DeokJong <novelss5573@gmail.com>
2026-06-13 18:55:05 +0900

    feat: addons를 IRSA, Pod Identity 방식으로 구성

---
+++ terraform/platform/eks-sandbox/iam-addons.tf
@@ -0,0 +1,25 @@
+module "cert_manager_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+  version = "~> 6.0"
+
+  name                          = "cert-manager"
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = var.route53_arns
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.sandbox.oidc_provider_arn
+      namespace_service_accounts = ["cert-manager:cert-manager"]
+    }
+  }
+}
+
+module "external_dns_pod_identity" {
+  source  = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "~> 2.0"
+
+  name                          = "external_dns"
+  use_name_prefix               = "false"
+  attach_external_dns_policy    = true
+  external_dns_hosted_zone_arns = var.route53_arns
+}
