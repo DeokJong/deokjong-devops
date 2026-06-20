aa9e6fada1a3d0737ff585d6f4bfa39b750892df
DeokJong <novelss5573@gmail.com>
2026-06-20 16:12:48 +0900

    chore: root-appset 으로 변경

--- modules/platform/eks/bootstrap/main.tf
+++ modules/platform/eks/bootstrap/main.tf
@@ -26,22 +26,22 @@ resource "kubernetes_manifest" "node_pools" {
   manifest = yamldecode(templatefile("${path.module}/manifests/nodepools.yaml.tftpl", each.value))
 }
 
-resource "kubernetes_manifest" "root_application" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/root-application.yaml.tftpl", {
+resource "kubernetes_manifest" "applicationset" {
+  manifest = yamldecode(templatefile("${path.module}/manifests/applicationset.yaml.tftpl", {
     cluster_name = var.cluster_name
   }))
 }
 
 resource "kubernetes_secret_v1" "cluster" {
   metadata {
-    name      = var.cluster_name
+    name      = "in-cluster"
     namespace = "argocd"
     labels = {
       "argocd.argoproj.io/secret-type" = "cluster"
     }
   }
   data = {
-    name    = var.cluster_name
+    name    = "in-cluster"
     server  = var.cluster_arn
     project = "default"
   }
