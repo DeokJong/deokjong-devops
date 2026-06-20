6cd7d6917fd82e1a52dccd52458a017771c6e843
DeokJong <novelss5573@gmail.com>
2026-06-20 16:53:40 +0900

    chore: nodepools 변경

--- modules/platform/eks/addons/main.tf
+++ modules/platform/eks/addons/main.tf
@@ -1,24 +1,9 @@
-locals {
-  critical_affinity = {
-    nodeSelector = {
-      "karpenter.sh/nodepool" = "critical"
-    }
-    tolerations = [
-      { key = "CriticalAddonsOnly", operator = "Exists", effect = "NoSchedule" }
-    ]
-  }
-}
-
 resource "aws_eks_addon" "metrics_server" {
   cluster_name                = var.cluster_name
   addon_name                  = "metrics-server"
   preserve                    = false
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
-  configuration_values = jsonencode({
-    tolerations  = local.critical_affinity.tolerations
-    nodeSelector = local.critical_affinity.nodeSelector
-  })
 }
 
 resource "aws_eks_addon" "cert_manager" {
@@ -28,8 +13,6 @@ resource "aws_eks_addon" "cert_manager" {
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
   configuration_values = jsonencode({
-    tolerations  = local.critical_affinity.tolerations
-    nodeSelector = local.critical_affinity.nodeSelector
     extraArgs = [
       "--dns01-recursive-nameservers-only",
       "--dns01-recursive-nameservers=1.1.1.1:53,8.8.8.8:53",
@@ -44,8 +27,6 @@ resource "aws_eks_addon" "external_dns" {
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
   configuration_values = jsonencode({
-    tolerations  = local.critical_affinity.tolerations
-    nodeSelector = local.critical_affinity.nodeSelector
     txtOwnerId   = var.cluster_name
   })
 
